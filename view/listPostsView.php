744c867ac989712437db55f5230502654d54c6d0
08rose08 <naudinrose@gmail.com>
2020-09-16 19:50:19 +0200

    Add style

--- view/listPostsView.php
+++ view/listPostsView.php
@@ -4,18 +4,26 @@
 
 <?php ob_start(); ?>
 
-<h1>La liste des recettes</h1>
+<header class="masthead-list">
+    <div class="container d-flex h-20 align-items-center">
+        
+        <div class="mx-auto text-center">
+            <h1 class="mx-auto text-uppercase">Le blog-recettes</h1>
+        </div>
+    </div>
+</header>
 
-<?php
-foreach ($posts as $post){
-?>
-    
-    <h2><a href='index.php?action=getOnePost&amp;id=<?php echo $post->id() ?>'><?php echo $post->title() ?></a></h2>
-    <p><?php echo $post->chapo() ?></p>
-    <p>Par <?php echo $post->name() ?> le <?php echo $post->last_update_date() ?></p>
-
-<?php } ?>
-
+<main class="bg-black p-3">
+    <?php
+    foreach ($posts as $post){
+    ?>
+        <div class="card bg-light m-3 p-3">
+            <h2><a href='index.php?action=getOnePost&amp;id=<?php echo $post->id() ?>'><?php echo $post->title() ?></a></h2>
+            <p><?php echo $post->chapo() ?></p>
+            <p>Par <?php echo $post->name() ?> le <?php echo $post->last_update_date() ?></p>
+        </div>
+    <?php } ?>
+</main>
 <?php $content = ob_get_clean(); ?>
 
 <?php require 'template.php'; ?>
