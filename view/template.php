a2e85c515f0ee7c322937dc854ed23b738b4b632
08rose08 <naudinrose@gmail.com>
2020-10-16 18:12:54 +0200

    Fix Codacy add htmlentities() in template

--- view/template.php
+++ view/template.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="Rose Naudin" />
-        <title><?= addslashes($title) ?></title>
+        <title><?= htmlentities($title) ?></title>
         <!-- Favicon-->
         <link rel="icon" type="image/x-icon" href="public/img/favicon.ico" />
         <!-- Font Awesome icons (free version)-->
