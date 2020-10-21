a2d7d37a3ac4c9915d67bfea56b3b14665715a3b
08rose08 <naudinrose@gmail.com>
2020-10-21 17:38:22 +0200

    Fix lg-modal in buttonPostAdminView

--- view/buttonsPostAdminView.php
+++ view/buttonsPostAdminView.php
@@ -29,8 +29,8 @@
 </div>
 
 
-<div class="modal fade" id="postUpdateModal" tabindex="-1" role="dialog" aria-labelledby="modalDeletePostTitle" aria-hidden="true">
-  <div class="modal-dialog" role="document">
+<div class="modal fade w-100" id="postUpdateModal" tabindex="-1" role="dialog" aria-labelledby="modalDeletePostTitle" aria-hidden="true">
+  <div class="modal-dialog modal-lg" role="document">
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title" id="modalDeletePostTitle">Modifier le post</h5>
