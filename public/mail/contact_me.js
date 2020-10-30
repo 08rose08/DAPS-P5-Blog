dedc177f23e093388b830131f8f82f25c05721d1
08rose08 <naudinrose@gmail.com>
2020-10-30 11:23:43 +0100

    Fix contact_me

--- public/mail/contact_me.js
+++ public/mail/contact_me.js
@@ -20,7 +20,7 @@ $(function () {
             $this = $("#sendMessageButton");
             $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
             $.ajax({
-                url: "/DAPS-P5-Blog/public/mail/contact_me.php",
+                url: "/public/mail/contact_me.php",
                 type: "POST",
                 data: {
                     name: name,
